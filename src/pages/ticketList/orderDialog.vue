d5402d17372b94b7179e972061c6817dce99365a
caizhibin <caizhibin@cleartv.cn>
2017-06-30 14:05:31 +0800

    同游人信息可填可不填，手机和身份证若填写则增加验证。

--- src/pages/ticketList/orderDialog.vue
+++ src/pages/ticketList/orderDialog.vue
@@ -1,7 +1,6 @@
 <template>
   <!--弹出-->
   <el-dialog :visible.sync="dialogFormVisible" title="下单">
-    <button @click="filterTourist">下单咯</button>
     <el-form ref="form" :rules="rules" :model="form" label-width="120px">
       <div class="row">
         <div class="col-xs-12">
@@ -48,19 +47,20 @@
               <el-input v-model="item.name" placeholder="请输入姓名"></el-input>
             </el-form-item>
 
-            <el-form-item :label="'同游人'+(index + 1)+'手机号'">
+            <el-form-item :label="'同游人'+(index + 1)+'手机号'" :key="index" :prop="'travelerList.'+index+'.mobile'"
+                          :rules="rules2.mobile">
               <el-input v-model="item.mobile" placeholder="请输入手机号"></el-input>
             </el-form-item>
 
-            <el-form-item :label="'同游人'+(index + 1)+'证件号'">
+            <el-form-item :label="'同游人'+(index + 1)+'证件号'" :key="index" :prop="'travelerList.'+index+'.idNum'"
+                          :rules="rules2.idType">
               <el-input placeholder="请输入证件号" v-model="item.idNum">
-                <el-select v-model="item.idType" placeholder="请选择" slot="prepend">
+                <el-select v-model="item.idType" placeholder="请选择" slot="prepend" @change="selectChange2(index)">
                   <el-option v-for="item in form.selectOption" :key="item.value" :label="item.label"
                              :value="item.value"></el-option>
                 </el-select>
               </el-input>
             </el-form-item>
-
           </div>
         </el-collapse-item>
       </el-collapse>
@@ -137,6 +137,33 @@
           callback()
         }
       }
+
+      var validateMobile2 = (rule, value, callback) => {
+        if (value === '') {
+          callback()
+        }
+        if (!(/^1[34578]\d{9}$/.test(value))) {
+          callback(new Error('请输入正确的手机号码!'))
+        } else {
+          callback()
+        }
+      }
+      var validateId2 = (rule, value, callback) => {
+        let sels = rule.field.split('.')
+        let sel = sels[1]  // 获取当前索引
+        if (value === '') {
+          callback()
+        }
+        if (this.form.travelerList[sel].idType === 'ID_CARD') {
+          if (!(/^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(value))) {
+            callback(new Error('请输入正确的身份证号!'))
+          } else {
+            callback()
+          }
+        } else {
+          callback()
+        }
+      }
       return {
         dialogFormVisible: false,
         disabled: false,
@@ -167,6 +194,10 @@
             {required: true, message: '请输入证件号', trigger: 'blur,change'},
             {validator: validateId, trigger: 'blur,change'}
           ]
+        },
+        rules2: {
+          idType: {validator: validateId2, trigger: 'blur,change'},
+          mobile: {validator: validateMobile2, trigger: 'blur,change'}
         }
       }
     },
@@ -235,6 +266,9 @@
       },
       selectChange () {
         this.$refs.form.validateField('contactsIdNum')
+      },
+      selectChange2 (index) {
+        this.$refs.form.validateField('travelerList.' + index + '.idNum')
       },
       submitForm (formName) {
         this.$refs[formName].validate((valid) => {
